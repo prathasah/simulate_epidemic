be207b649d6a935cb27d8b29a3e0b814563e112d
prathasah <ps875@georgetown.edu>
2013-08-26 11:26:22 -0400

    Added descriptions and updated

--- epidemic_spread.py
+++ epidemic_spread.py
@@ -10,13 +10,16 @@ import module_struct_12June2013 as gr
 
 def run_timebased_epidemic(G,beta,sigma):
     """N=network size, T=transmissibility, tau=recovery probability"""
-    start=time.clock()
     N=len(G.nodes())
-    infected_list=[0]*N
-    recovered_list = [0]*N
-    track_infection=[0]*N
-    track_recovery=[0]*N
-    p_zero = rnd.choice(G.nodes())
+    #infected_list is a list that contains zeros and ones. 
+    #Zero indicates that the node is susceptibile (or recovered). One indicates that the node is infected
+    infected_list=[0]*N #Initialize list with zeros indicating all nodes are susceptibles
+    #recovered_list is a list that contains zeros and ones. 
+    #Zero indicates that the node has not recovered. One indicates that the node has recovered
+    recovered_list = [0]*N #Initialize list with zeros indicating none of the nodes are recovered at t=0
+    track_infection=[0]*N #tracks time at infection for each node.
+    track_recovery=[0]*N  #tracks time of recovery for each node.
+    p_zero = rnd.choice(G.nodes()) # choose a random node to infect
     track_time=1
     infected_list[p_zero] = 1
     track_infection[p_zero]=1
@@ -25,16 +28,21 @@ def run_timebased_epidemic(G,beta,sigma):
         #print ("time=="), time
         #visualize_epidemic(G, pos, infected_list, recovered_list, track_time)
         track_time += 1
-        infected_list_new = [1 if infected_list[node]==0 and recovered_list[node]==0 and rnd.random()< (1- np.exp(-beta*infected_neighbors(node, infected_list))) else infected_list[node] for node in xrange(N)]       
-        track_infection=[track_time if infected_list_new[node]-infected_list[node]==1 and recovered_list[node]==0 else track_infection[node] for node in xrange(N)]
+        # infect the susceptibles with the probability 1-exp(-bk) where k is the number of infeced neighbors.
+        infected_list_new = [1 if not infected_list[node] and not recovered_list[node] and rnd.random()< (1- np.exp(-beta*infected_neighbors(G, node, infected_list))) else infected_list[node] for node in xrange(N)]       
+        #Store the time of infection for the newly infected nodes in track_infection list
+        track_infection=[track_time if infected_list_new[node]-infected_list[node]==1 and not recovered_list[node] else track_infection[node] for node in xrange(N)]
+        # update the original infected_list
         infected_list=[infected_list_new[x] if infected_list_new[x]==1 else infected_list[x] for x in xrange(N)]
+        #All the infected nodes recover with a probailty sigma
         recovered_list_new=[1 if infected_list[node]==1 and rnd.random()<sigma else 0 for node in xrange (N) ]
+        # Store the time of recovery for the newly recovered nodes in track_recovered list
         track_recovery=[track_time if recovered_list_new[node]-recovered_list[node]==1 else track_recovery[node] for node in xrange(N)] 
+        #update infected_list
         infected_list=[0 if recovered_list_new[node]==1 else infected_list[node] for node in xrange(N)]
+        # update orignial recovered_list
         recovered_list=[recovered_list_new[x] if recovered_list_new[x]==1 else recovered_list[x] for x in xrange(N)]      
     
-    print ("time to extinction"),track_time
-    print ("time taken infection"), time.clock()-start
     return track_infection, track_recovery   
         
         
@@ -50,18 +58,26 @@ def visualize_epidemic(G, pos, infected_list, recovered_list, track_time):
     plt.title("time=" + str(time) + ", susceptible= " + str(len(susceptible_list))+", infected= "+str(sum(infected_list))+ ", recovered= "+str(sum(recovered_list)))
     plt.savefig('modular_time'+str(time).zfill(3)+'.png')
 
-def infected_neighbors(node, infected_list):
-    """Calculated the number of infected neighbors for the node"""
+def infected_neighbors(G, node, infected_list):
+    """Calculates the number of infected neighbors for the node"""
     return sum([infected_list[node_i] for node_i in G.neighbors(node)])   
     
         
 if __name__ == "__main__":
-    #G=gr.generate_mod_networks("poisson", 0.6, 1000, 10, 5)
+    """Main function to mimic C++ behavior. This function requires the file
+    random-modular-network-generator.py."""
+    #try:
+    print " Starting a epidemic simulation on a simple poisson modular random graph with modularity 0.6"
+    print "The graph has 1000 nodes, 10 modules and has has average network degree of 5"
+    print " Starting the epidemic simulation"
+    #G=gr.generate_mod_networks("poisson", 0.6, 10000, 10, 5)
     G=nx.read_edgelist("Edge_list_poissonn10000_m10_d10_Q0.0_iter0.txt", nodetype=int, data=False)
-    track_infection, track_recovery = run_timebased_epidemic(G,0.061, 0.143)
-    #print ("time to extinction"), time
-    print ("tracked recovery"), track_recovery
-    #print ("track recovery"), track_recovery
-    #incidence= ec.calculate_incidence (time, track_infected)
-    #print ("incidence"), incidence
+    start=time.time()
+    track_infection, track_recovery = run_timebased_epidemic(G,0.025, 0.143)
+    print ("time"), time.time()-start
+    #print ("list of nodal infection time "), track_infection
+    #print ("list of nodal reovery time "), track_recovery
+    #except (IndexError, IOError):
+    #    print "Reached an errror. Try again"
+
         
